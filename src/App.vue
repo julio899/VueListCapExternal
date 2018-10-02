e6f19147121638eafecee9f5259f5f3fed6ecd86
Julio Vinachi <julio899@gmail.com>
2018-10-02 00:32:28 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="applist">
+    <ListModule msg="modulo para la generacion de listas"/>
+  </div>
+</template>
+
+<script>
+import ListModule from './components/ListModule.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ListModule
+  }
+}
+</script>
+
+<style>
+#applist {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+</style>
